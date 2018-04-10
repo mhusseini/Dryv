9b07720f670cb60696c21ff3a947504bf5f7fea5
Munir Husseini <m.husseini@gmx.de>
2018-04-10 22:51:27 +0200

    Fixed options replacement in translated code when multiple option lambdas use the same option type.

---
+++ Dryv/Translation/TranslationCompiler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Dryv.Translation
+{
+    internal class TranslationCompiler
+    {
+        private static readonly MethodInfo FormatMethod = typeof(string).GetMethod(nameof(string.Format), new[] { typeof(string), typeof(object[]) });
+
+        private static readonly MethodInfo TranslateValueMethod = typeof(Translator).GetMethod(nameof(Translator.TranslateValue));
+        private readonly object translator;
+
+        public TranslationCompiler(object translator)
+        {
+            this.translator = translator;
+        }
+
+        public TranslationResult GenerateTranslationDelegate(string code, List<LambdaExpression> optionDelegates, List<Type> optionTypes)
+        {
+            // Escape curly braces for usage within string.Format().
+            code = code
+                .Replace("{", "{{")
+                .Replace("}", "}}");
+
+            // We will produce a delegate that has a single parameter:
+            var parameter = Expression.Parameter(typeof(object[]));
+
+            // Create an array that will be used as parameters for string.Format().
+            var arrayItems = this.GenerateFormatArgumentExpressions(optionDelegates, optionTypes, parameter, ref code);
+
+            // create the following code: string.Format("...", new[]{ ... })
+            var pattern = Expression.Constant(code);
+            var arguments = Expression.NewArrayInit(typeof(object), arrayItems);
+            var format = Expression.Call(null, FormatMethod, pattern, arguments);
+            var result = Expression.Lambda<Func<object[], string>>(format, parameter);
+
+            return new TranslationResult
+            {
+                Factory = result.Compile(),
+                OptionTypes = optionTypes.ToArray()
+            };
+        }
+
+        private static IEnumerable<Type> GetTypeChain(Expression expression)
+        {
+            if (!(expression is MemberExpression memberExpression))
+            {
+                yield break;
+            }
+
+            while (memberExpression != null)
+            {
+                switch (memberExpression.Expression)
+                {
+                    case MemberExpression mex:
+                        memberExpression = mex;
+                        yield return mex.Type;
+                        break;
+
+                    case ParameterExpression pex:
+                        memberExpression = null;
+                        yield return pex.Type;
+                        break;
+                }
+            }
+        }
+
+        private IEnumerable<Expression> GenerateFormatArgumentExpressions(
+            IEnumerable<LambdaExpression> optionDelegates,
+            IList<Type> optionTypes,
+            Expression parameter,
+            ref string code)
+        {
+            var arrayItems = new List<Expression>();
+            var arrayIndexes = new ConcurrentDictionary<Type, int>();
+
+            foreach (var lambda in optionDelegates)
+            {
+                var optionType = GetTypeChain(lambda.Body).Last();
+                var index = arrayIndexes.GetOrAdd(optionType, t =>
+                {
+                    var idx = optionTypes.IndexOf(optionType);
+                    // get item from input array (properly casted)
+                    var p = Expression.Convert(Expression.ArrayAccess(parameter, Expression.Constant(idx)), optionType);
+                    // invoke lambda with item as argument
+                    var optionValue = Expression.Convert(Expression.Invoke(lambda, p), typeof(object));
+                    // translate result of lambda to JavaScript
+                    var translatedOptionsValue = Expression.Call(Expression.Constant(this.translator), TranslateValueMethod, optionValue);
+                    // Add the whole expression to the formatting array
+                    arrayItems.Add(Expression.Convert(translatedOptionsValue, typeof(object)));
+
+                    return idx;
+                });
+
+                code = code.Replace($"$${lambda.GetHashCode()}$$", $"{{{index}}}");
+            }
+
+            {
+                var index = arrayIndexes.Any() ? arrayIndexes.Values.Max() + 1 : 0;
+                var modelPathParameter = Expression.ArrayAccess(parameter, Expression.Constant(index));
+                // Put that item into the formatting array
+                arrayItems.Add(modelPathParameter);
+                // Replace all occurences of $$MODELPATH$$ with the appropriate formatting placeholder
+                code = code.Replace("$$MODELPATH$$", $"{{{index}}}");
+            }
+
+            return arrayItems;
+        }
+    }
+}
--- Dryv/Translation/Translator.cs
+++ Dryv/Translation/Translator.cs
@@ -2,115 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Text;
 
 namespace Dryv.Translation
 {
     public abstract class Translator : ITranslator
     {
-        private static readonly MethodInfo TranslateValueMethod = typeof(Translator).GetMethod(nameof(TranslateValue));
+        private readonly TranslationCompiler translationCompiler;
+
+        protected Translator()
+        {
+            this.translationCompiler = new TranslationCompiler(this);
+        }
 
         public virtual TranslationResult Translate(Expression expression, MemberExpression propertyExpression)
         {
-            var sb = new StringBuilder();
-            var optionDelegates = new List<LambdaExpression>();
-            var optionTypes = GetOptionTypes(expression);
-
-            using (var writer = new IndentingStringWriter(sb))
-            {
-                var context = new TranslationContext
-                {
-                    OptionsTypes = optionTypes,
-                    Writer = writer,
-                    OptionDelegates = optionDelegates,
-                    ModelType = propertyExpression?.Expression.GetExpressionType(),
-                    PropertyExpression = propertyExpression?.Expression
-                };
-
-                this.Translate(expression, context);
-            }
-
-            //if (!optionDelegates.Any())
-            //{
-            //    var lambda = Expression.Lambda<Func<object[], string>>(
-            //        Expression.Constant(sb.ToString()),
-            //        Expression.Parameter(typeof(object[])));
-
-            //    return new TranslationResult
-            //    {
-            //        Factory = lambda.Compile(),
-            //        OptionTypes = optionTypes.ToArray()
-            //    };
-            //}
-
-            var code = sb.ToString()
-                .Replace("{", "{{")
-                .Replace("}", "}}");
-            var parameter = Expression.Parameter(typeof(object[]));
-            // Create an array that will be used as parameters for string.Format 
-            var arrayItems = new List<Expression>();
-
-            foreach (var lambda in optionDelegates)
-            {
-                var optionType = this.GetTypeChain(lambda.Body).Last();
-                var idx = optionTypes.IndexOf(optionType);
-                code = code.Replace($"$${lambda.GetHashCode()}$$", $"{{{idx}}}");
-                // get item from input array (properly casted)
-                var p = Expression.Convert(Expression.ArrayAccess(parameter, Expression.Constant(idx)), optionType);
-                // invoke lambda with item as argument
-                var optionValue = Expression.Convert(Expression.Invoke(lambda, p), typeof(object));
-                // translate result of lambda to JavaScript
-                var translatedOptionsValue = Expression.Call(Expression.Constant(this), TranslateValueMethod, optionValue);
-                // Add the whole expression to the formatting array
-                arrayItems.Add(Expression.Convert(translatedOptionsValue, typeof(object)));
-            }
-
-            {
-                // Last item in input array is the path of the current model.
-                var idx = optionTypes.Count;
-                var modelPathParameter = Expression.ArrayAccess(parameter, Expression.Constant(idx));
-                // Put that item into the formatting array 
-                arrayItems.Add(modelPathParameter);
-                // Replace all occurences of $$MODELPATH$$ with the appropriate formatting placeholder 
-                code = code.Replace("$$MODELPATH$$", $"{{{idx}}}");
-            }
-
-            // create the following code: string.Format("...", new[]{ ... })
-            var formatMethod = typeof(string).GetMethod(nameof(string.Format), new[] { typeof(string), typeof(object[]) });
-            var pattern = Expression.Constant(code);
-            var array = Expression.NewArrayInit(typeof(object), arrayItems);
-            var format = Expression.Call(null, formatMethod, pattern, array);
-            var result = Expression.Lambda<Func<object[], string>>(format, parameter);
-
-            return new TranslationResult
-            {
-                Factory = result.Compile(),
-                OptionTypes = optionTypes.ToArray()
-            };
-        }
-
-        private IEnumerable<Type> GetTypeChain(Expression expression)
-        {
-            if (!(expression is MemberExpression memberExpression))
-            {
-                yield break;
-            }
-
-            while (memberExpression != null)
-            {
-                switch (memberExpression.Expression)
-                {
-                    case MemberExpression mex:
-                        memberExpression = mex;
-                        yield return mex.Type;
-                        break;
-                    case ParameterExpression pex:
-                        memberExpression = null;
-                        yield return pex.Type;
-                        break;
-                }
-            }
+            var (code, optionTypes, optionDelegates) = this.GenerateJavaScriptCode(expression, propertyExpression);
+            return this.translationCompiler.GenerateTranslationDelegate(code, optionDelegates, optionTypes);
         }
 
         public virtual void Translate(Expression expression, TranslationContext context)
@@ -247,9 +155,43 @@ namespace Dryv.Translation
         private static List<Type> GetOptionTypes(Expression expression)
         {
             var lambda = (LambdaExpression)expression;
-            var args = lambda.Type.GetGenericArguments().Skip(1).ToList();
-            args = args.Take(args.Count - 1).ToList();
-            return args;
+            var genericArguments = lambda.Type.GetGenericArguments();
+            return genericArguments
+                .Skip(1)
+                .Take(genericArguments.Length - 2)
+                .ToList();
+        }
+
+        private (string Code, List<Type> OptionTypes, List<LambdaExpression> OptionDelegates) GenerateJavaScriptCode(
+            Expression expression,
+            MemberExpression propertyExpression)
+        {
+            // Find all option types used in the validation expression.
+            var optionTypes = GetOptionTypes(expression);
+            // Collect delegates that use options from withing the validation expression.
+            var optionDelegates = new List<LambdaExpression>();
+            var sb = new StringBuilder();
+
+            using (var writer = new IndentingStringWriter(sb))
+            {
+                var context = new TranslationContext
+                {
+                    OptionsTypes = optionTypes,
+                    Writer = writer,
+                    OptionDelegates = optionDelegates,
+                    ModelType = propertyExpression?.Expression.GetExpressionType(),
+                    PropertyExpression = propertyExpression?.Expression
+                };
+
+                this.Translate(expression, context);
+            }
+
+            return
+            (
+                Code: sb.ToString(),
+                OptionTypes: optionTypes,
+                OptionDelegates: optionDelegates
+            );
         }
     }
 }
