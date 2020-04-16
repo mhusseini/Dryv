6f2d5642bca801b506986ed3181ce0674673e51a
Munir Husseini <mhusseini@softwareproduction.eu>
2020-04-16 11:55:02 +0200

    Fixed detection of validatable types

--- Dryv.AspNetCore/Internal/DryvValidationFilterAttribute.cs
+++ Dryv.AspNetCore/Internal/DryvValidationFilterAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace Dryv.Internal
 {
     /// <inheritdoc />
     /// <summary>
-    /// As long as mvc validation is not async, we'll 
+    /// As long as mvc validation is not async, we'll
     /// run the async validation from an action attribute.
     /// </summary>
     internal class DryvValidationFilterAttribute : IAsyncActionFilter
@@ -26,10 +27,39 @@ namespace Dryv.Internal
             await next();
         }
 
+        private static IEnumerable<PropertyInfo> GetAllProperties(Type type) => GetAllProperties(type, new HashSet<PropertyInfo>());
+
+        private static void GetAllProperties(PropertyInfo property, HashSet<PropertyInfo> list)
+        {
+            if (list.Contains(property))
+            {
+                return;
+            }
+
+            list.Add(property);
+
+            GetAllProperties(property.PropertyType, list);
+        }
+
+        private static HashSet<PropertyInfo> GetAllProperties(Type type, HashSet<PropertyInfo> list)
+        {
+            if (IsPrimitive(type))
+            {
+                return list;
+            }
+
+            foreach (var p in type.GetProperties())
+            {
+                GetAllProperties(p, list);
+            }
+
+            return list;
+        }
+
+        private static bool IsPrimitive(Type t) => t.IsPrimitive || t == typeof(string);
+
         private static bool ShouldValidate(object a) => ShouldValidate(a?.GetType());
 
-        private static bool ShouldValidate(Type t) => t?.IsPrimitive == false
-                                                      && t != typeof(string)
-                                                      && t.GetProperties().Any(p => p.GetCustomAttributes<DryvRulesAttribute>().Any());
+        private static bool ShouldValidate(Type t) => GetAllProperties(t).SelectMany(p => p.GetCustomAttributes<DryvRulesAttribute>()).Any();
     }
 }
