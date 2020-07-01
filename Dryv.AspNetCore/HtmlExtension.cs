74d3af24bd693df8cb47d9fdc7d664662ac649c8
Munir Husseini <mhusseini@softwareproduction.eu>
2020-07-01 03:18:35 +0200

    Refactored code

--- Dryv.AspNetCore/HtmlExtension.cs
+++ Dryv.AspNetCore/HtmlExtension.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
+using Dryv.AspNetCore.Razor;
 using Dryv.Extensions;
 using Dryv.Validation;
 using Microsoft.AspNetCore.Html;
@@ -12,29 +13,29 @@ namespace Dryv.AspNetCore
 {
     public static class HtmlExtensions
     {
-        public static DryvClientValidationItem DryvClientValidationFor<TModel>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, object>> propertyExpression) => DryvClientValidationFor((IHtmlHelper)htmlHelper, propertyExpression);
+        public static IHtmlContent DryvValidation<TModel>(this IHtmlHelper<TModel> htmlHelper, string validationSetName) => htmlHelper.GetDryvClientWriter().WriteDryvValidation<TModel>(validationSetName);
 
-        public static DryvClientValidationItem DryvClientValidationFor<TModel>(this IHtmlHelper htmlHelper, Expression<Func<TModel, object>> propertyExpression) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidation(propertyExpression);
+        public static IHtmlContent DryvValidation<TModel>(this IHtmlHelper htmlHelper, string validationSetName) => htmlHelper.GetDryvClientWriter().WriteDryvValidation<TModel>(validationSetName);
 
-        public static IDictionary<string, Action<TextWriter>> GetDryvClientDisablingFunctions<TModel>(this IHtmlHelper<TModel> htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientDisablingFunctions<TModel>();
+        public static IHtmlContent DryvValidation<TModel>(this IHtmlHelper htmlHelper) => DryvValidation<TModel>(htmlHelper, typeof(TModel).Name.ToCamelCase());
 
-        public static IDictionary<string, Action<TextWriter>> GetDryvClientDisablingFunctions<TModel>(this IHtmlHelper htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientDisablingFunctions<TModel>();
+        public static IHtmlContent DryvValidation<TModel>(this IHtmlHelper<TModel> htmlHelper) => DryvValidation<TModel>((IHtmlHelper)htmlHelper);
+
+        public static IDictionary<string, IHtmlContent> GetDryvClientDisablingFunctions<TModel>(this IHtmlHelper<TModel> htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientDisablingFunctions<TModel>().ToDictionary(i => i.Key, i => (IHtmlContent)new LazyHtmlContent(i.Value));
+
+        public static IDictionary<string, IHtmlContent> GetDryvClientDisablingFunctions<TModel>(this IHtmlHelper htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientDisablingFunctions<TModel>().ToDictionary(i => i.Key, i => (IHtmlContent)new LazyHtmlContent(i.Value));
 
         public static IEnumerable<DryvClientValidationItem> GetDryvClientValidation<TModel>(this IHtmlHelper<TModel> htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidation<TModel>();
 
         public static IEnumerable<DryvClientValidationItem> GetDryvClientValidation<TModel>(this IHtmlHelper htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidation<TModel>();
 
-        public static IDictionary<string, Action<TextWriter>> GetDryvClientValidationFunctions<TModel>(this IHtmlHelper<TModel> htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidationFunctions<TModel>();
+        public static DryvClientValidationItem GetDryvClientValidationFor<TModel>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, object>> propertyExpression) => GetDryvClientValidationFor((IHtmlHelper)htmlHelper, propertyExpression);
 
-        public static IDictionary<string, Action<TextWriter>> GetDryvClientValidationFunctions<TModel>(this IHtmlHelper htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidationFunctions<TModel>();
+        public static DryvClientValidationItem GetDryvClientValidationFor<TModel>(this IHtmlHelper htmlHelper, Expression<Func<TModel, object>> propertyExpression) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidation(propertyExpression);
 
-        public static IHtmlContent WriteDryvValidation<TModel>(this IHtmlHelper<TModel> htmlHelper, string validationSetName) => htmlHelper.GetDryvClientWriter().WriteDryvValidation<TModel>(validationSetName);
+        public static IDictionary<string, IHtmlContent> GetDryvClientValidationFunctions<TModel>(this IHtmlHelper<TModel> htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidationFunctions<TModel>().ToDictionary(i => i.Key, i => (IHtmlContent)new LazyHtmlContent(i.Value));
 
-        public static IHtmlContent WriteDryvValidation<TModel>(this IHtmlHelper htmlHelper, string validationSetName) => htmlHelper.GetDryvClientWriter().WriteDryvValidation<TModel>(validationSetName);
-
-        public static IHtmlContent WriteDryvValidation<TModel>(this IHtmlHelper htmlHelper) => WriteDryvValidation<TModel>(htmlHelper, typeof(TModel).Name.ToCamelCase());
-
-        public static IHtmlContent WriteDryvValidation<TModel>(this IHtmlHelper<TModel> htmlHelper) => WriteDryvValidation<TModel>((IHtmlHelper)htmlHelper);
+        public static IDictionary<string, IHtmlContent> GetDryvClientValidationFunctions<TModel>(this IHtmlHelper htmlHelper) => htmlHelper.GetDryvClientValidationLoader().GetDryvClientValidationFunctions<TModel>().ToDictionary(i => i.Key, i => (IHtmlContent)new LazyHtmlContent(i.Value));
 
         private static DryvClientValidationLoader GetDryvClientValidationLoader(this IHtmlHelper htmlHelper) => htmlHelper.ViewContext.HttpContext.RequestServices.GetService<DryvClientValidationLoader>();
 
--- Dryv/Validation/DryvAsyncClientValidationFunctionWriter.cs
+++ Dryv/Validation/DryvAsyncClientValidationFunctionWriter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using Dryv.RuleDetection;
 
 namespace Dryv.Validation
