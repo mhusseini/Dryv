2a551b2ad9bff2ab8ac534241f7a86c1f9243912
Munir Husseini <mhusseini@softewareproduction.eu>
2020-09-25 16:52:15 +0200

    Using MD5 for default controller URLs.

--- Dryv.AspNetCore/CustomizationDefaults.cs
+++ Dryv.AspNetCore/CustomizationDefaults.cs
@@ -1,4 +1,6 @@
-﻿using Dryv.AspNetCore.DynamicControllers;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Dryv.AspNetCore.DynamicControllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 
@@ -6,16 +8,30 @@ namespace Dryv.AspNetCore
 {
     public static class CustomizationDefaults
     {
-        private static int controllerCount;
-
         public static void DefaultEndpoint(DryvControllerGenerationContext context, IEndpointRouteBuilder builder) =>
             builder.MapControllerRoute(
                 context.ControllerFullName,
-                $"validation/{context.Controller}/{context.Action}",
+                DefaultRoute(context),
                 new { controller = context.Controller, action = context.Action });
 
         public static DryvDynamicControllerMethods DefaultHttpMethod(DryvControllerGenerationContext context) => DryvDynamicControllerMethods.Post;
 
-        public static string DefaultRoute(DryvControllerGenerationContext context) => $"_v/c{++controllerCount}";
+        public static string DefaultRoute(DryvControllerGenerationContext context) => $"_v/c{CreateMd5(context.ControllerFullName)}";
+
+        private static string CreateMd5(string input)
+        {
+            using var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hashBytes = md5.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            
+            foreach (var b in hashBytes)
+            {
+                sb.Append(b.ToString());
+            }
+            
+            return sb.ToString();
+        }
     }
 }
